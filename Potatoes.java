3e783be6cca4e8112a84011a79c0ee784352d301
Itx-Dev <71668732+Itx-Dev@users.noreply.github.com>
2023-05-16 12:54:42 -0400

    Add files via upload

---
+++ Potatoes.java
@@ -0,0 +1,18 @@
+public class Potatoes 
+{ 
+	public static void main(String[] args) { 
+	 int potatoNumber; 
+	 potatoNumber = 1; 
+	 while (potatoNumber < 8) {
+		 
+		 System.out.print(potatoNumber); 
+		 if (potatoNumber != 4) {
+		 System.out.print(" potato");
+		 }
+		 System.out.println(""); 
+		 potatoNumber = potatoNumber + 1; 
+		} 
+	 System.out.println("More"); 
+	 System.out.println("You're out!"); 
+	 } 
+} 
